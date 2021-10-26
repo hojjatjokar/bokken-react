3ee7653d3b639b91efec2da69504b2c2fa0c9cf9
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-10-26 21:19:45 +0330

    chore: add cypress directory to jest ignore

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ const { defaults } = require('jest-config');
 module.exports = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/cypress/'],
 };
